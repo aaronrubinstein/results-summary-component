f0bd7638bddaf3dccead82062d8ff4be936ec2d9
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-03-06 02:18:59 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<main>
+    <div class="result">
+
+    </div>
+    <div class="summary">
+        
+    </div>
+  
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
