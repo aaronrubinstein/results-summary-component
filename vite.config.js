d78fc14d089f6c3a5edbf567f2a5c2410a7d5bc1
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-03-11 21:50:50 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/results-summary-component/",
   plugins: [svelte()],
 })
